116b527c695e2335fddb3c431edaf57fb6100f82
Vladimir Shvab <shvab.vladimir@gmail.com>
2025-10-02 15:30:37 +0300

    bugfix:#139: enhance pagination classes by introducing a base class with offset validation

--- content/pagination.py
+++ content/pagination.py
@@ -1,13 +1,32 @@
 from collections import OrderedDict
 
+from rest_framework.exceptions import ValidationError
 from rest_framework.pagination import (
     LimitOffsetPagination,
     PageNumberPagination,
 )
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 
-class NewsLimitOffsetPagination(LimitOffsetPagination):
+class BaseLimitOffsetPagination(LimitOffsetPagination):
+    """Базовый класс limit offset пагинации с валидацией на максимум."""
+
+    default_limit = api_settings.PAGE_SIZE or 50
+    max_limit = 1000
+    MAX_OFFSET = 20_000
+
+    def get_offset(self, request):
+        """Добавляет проверку offset на максимум."""
+        offset = super().get_offset(request)
+        if offset > self.MAX_OFFSET:
+            raise ValidationError(
+                {'offset': f'Слишком большое значение (>{self.MAX_OFFSET}).'}
+            )
+        return offset
+
+
+class NewsLimitOffsetPagination(BaseLimitOffsetPagination):
     """Пагинация для новостей."""
 
     default_limit = 6
